71b3b6c80747c88ec78badd0512c5e7662983012
Samuel Iglesias Gonsálvez <siglesias@igalia.com>
2016-08-26 07:37:42 +0200

    i965: enable ARB_gpu_shader_fp64 for Ivybridge/Valleyview
    Signed-off-by: Samuel Iglesias Gonsálvez <siglesias@igalia.com>

--- src/mesa/drivers/dri/i965/intel_extensions.c
+++ src/mesa/drivers/dri/i965/intel_extensions.c
@@ -204,6 +204,7 @@ intelInitExtensions(struct gl_context *ctx)
       ctx->Extensions.ARB_derivative_control = true;
       ctx->Extensions.ARB_framebuffer_no_attachments = true;
       ctx->Extensions.ARB_gpu_shader5 = true;
+      ctx->Extensions.ARB_gpu_shader_fp64 = true;
       ctx->Extensions.ARB_shader_atomic_counters = true;
       ctx->Extensions.ARB_shader_atomic_counter_ops = true;
       ctx->Extensions.ARB_shader_clock = true;
