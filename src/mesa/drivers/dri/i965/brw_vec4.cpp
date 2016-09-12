be7e267ee67d8df69df95cffe45380f57c6b7518
Juan A. Suarez Romero <jasuarez@igalia.com>
2016-09-12 16:06:22 +0000

    i965/vec4: keep original type when dealing with null registers
    Keep the original type when dealing with null registers. Specially
    because we do no want to introduce an implicit conversion between
    types that could affect the conditional flags.
    
    This affects specially when the original type is DF, and we are working
    on Ivybridge/Valleyview.

--- src/mesa/drivers/dri/i965/brw_vec4.cpp
+++ src/mesa/drivers/dri/i965/brw_vec4.cpp
@@ -1992,6 +1992,7 @@ vec4_visitor::convert_to_hw_regs()
          case BAD_FILE:
             /* Probably unused. */
             reg = brw_null_reg();
+            reg.type = src.type;
             break;
 
          case MRF:
@@ -2042,6 +2043,7 @@ vec4_visitor::convert_to_hw_regs()
 
       case BAD_FILE:
          reg = brw_null_reg();
+         reg.type = dst.type;
          break;
 
       case IMM:
@@ -2212,6 +2214,8 @@ vec4_visitor::lower_simd_width()
          } else {
             if (inst->dst.file != ARF)
                dst = horiz_offset(inst->dst, channel_offset);
+            else
+               dst.type = inst->dst.type;
          }
          linst->dst = dst;
 
