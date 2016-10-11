6c5164f75dcedb9495f1a2e15fb7193ce9451fae
Juan A. Suarez Romero <jasuarez@igalia.com>
2016-10-11 15:05:36 +0000

    i965/gen7: expose OpenGL 4.2 on Haswell
    GL_ARB_vertex_attrib_64bit was the last piece missing.

--- src/mesa/drivers/dri/i965/intel_extensions.c
+++ src/mesa/drivers/dri/i965/intel_extensions.c
@@ -273,7 +273,7 @@ intelInitExtensions(struct gl_context *ctx)
    if (brw->gen >= 8)
       ctx->Const.GLSLVersion = 450;
    else if (brw->is_haswell)
-      ctx->Const.GLSLVersion = 400;
+      ctx->Const.GLSLVersion = 420;
    else if (brw->gen >= 6)
       ctx->Const.GLSLVersion = 330;
    else
